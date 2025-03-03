78ce614b39382e57377217bb71cb611194905d35
anselmo <anselmo.vecchio.jose@gmail.com>
2025-03-03 17:49:52 -0300

    Update back

--- cine/back/index.js
+++ cine/back/index.js
@@ -1,13 +1,12 @@
 const app = require("./src/server");
 const conDb = require("./src/config/conDb");
 
+const PORT = process.env.PORT || 3000;
 
 conDb().then(() => {
-    app.listen(3000 , () => {
-        console.log("próximamente estarán disponibles los datos de películas");
+    app.listen(PORT, () => {
+        console.log(`Servidor corriendo en el puerto ${PORT} - próximamente estarán disponibles los datos de películas`);
     });
 }).catch(err => {
     console.log("Error al conectar la base de datos", err);
 });
-
-    
