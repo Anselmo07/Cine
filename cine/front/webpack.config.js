3a8751d14e2489e7cd00a93408369a7f7539f0cd
anselmo <anselmo.vecchio.jose@gmail.com>
2025-03-04 14:53:47 -0300

    1

--- cine/front/webpack.config.js
+++ cine/front/webpack.config.js
@@ -1,6 +1,5 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const Dotenv = require("dotenv-webpack");
 const fs = require("fs");
 
 // Buscar todos los archivos HTML en la raíz del proyecto
@@ -28,23 +27,11 @@ module.exports = {
         ],
     },
     plugins: [
-        new HtmlWebpackPlugin({
-          template: './index.html',  // ruta de tu archivo index.html
-          filename: 'index.html',  // genera el archivo index.html en dist/
-        }),
-        new HtmlWebpackPlugin({
-            template: './About.html', // Ruta del archivo About.html
-            filename: 'About.html',   // Generará About.html en dist/
-          }),
-          new HtmlWebpackPlugin({
-            template: './HistoriaCine.html',
-            filename: 'HistoriaCine.html',
-          }),
-          // Agrega más configuraciones similares si tienes otros archivos HTML
-          new HtmlWebpackPlugin({
-            template: './formulario.html',
-            filename: 'formulario.html',
-          }),
+        ...htmlFiles.map(file => new HtmlWebpackPlugin({
+            template: `./${file}`,
+            filename: file,  // Genera cada HTML en dist/
+        })),
     ],
 };
+
 
