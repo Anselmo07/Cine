4be0f099bb4b84815d5447c9cd268d236c92c905
anselmo <anselmo.vecchio.jose@gmail.com>
2025-03-08 19:49:21 -0300

    Back

--- cine/back/src/server.js
+++ cine/back/src/server.js
@@ -5,12 +5,20 @@ const cors = require("cors");
 
 const app = express();
 
+// Habilitar logging de peticiones HTTP
 app.use(morgan("dev"));
+
+// Configuración CORS para permitir solicitudes desde el dominio frontend
 app.use(cors({
-    origin: 'cine-sandy.vercel.app' 
+    origin: 'https://cine-atuxthdcx-anselmos-projects-96233391.vercel.app', // Especifica el origen permitido
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos permitidos
+    allowedHeaders: ['Content-Type', 'Authorization'], // Cabeceras permitidas
 }));
+
+// Middleware para parsear el cuerpo de las solicitudes como JSON
 app.use(express.json());
 
+// Configuración de rutas
 app.use(router);
 
 module.exports = app;
